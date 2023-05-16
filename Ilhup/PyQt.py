8970f9e31a1acce112987167b3792ffb3efedf1e
unknown <guillaume.nguyenthi@gmail.com>
2023-05-17 01:44:01 +0200

    2.0

--- Ilhup/PyQt.py
+++ Ilhup/PyQt.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLabel, QPushButton, QWidget, QSpacerItem, QSizePolicy
+from PyQt5.QtCore import Qt
+
 
 class HomePage(QWidget):
     def __init__(self):
@@ -9,26 +11,35 @@ class HomePage(QWidget):
     def initUI(self):
         self.setWindowTitle('Home Page')
         self.setGeometry(300, 300, 300, 200)
+        spacer_item = QSpacerItem(20, 40)  # Add vertical spacing
+
 
         layout = QVBoxLayout()
 
-        label = QLabel('Welcome to the Home Page!', self)
+        layout.addItem(spacer_item)
+        label = QLabel('Choisissez le type de traitement :', self)
+        label.setStyleSheet("font-size: 20px; font-weight: bold;")
+        label.setAlignment(Qt.AlignCenter)  # Center-align the text
         layout.addWidget(label)
 
-        option1_button = QPushButton('Option 1', self)
+        layout.addItem(spacer_item)
+
+        option1_button = QPushButton('Fichiers IS', self)
         option1_button.setStyleSheet('QPushButton {background-color: #4CAF50; color: white; font-size: 18px;}')
         option1_button.clicked.connect(self.option1Clicked)
         layout.addWidget(option1_button)
 
-        option2_button = QPushButton('Option 2', self)
+        option2_button = QPushButton('Fichiers RTCP', self)
         option2_button.setStyleSheet('QPushButton {background-color: #f44336; color: white; font-size: 18px;}')
         option2_button.clicked.connect(self.option2Clicked)
         layout.addWidget(option2_button)
 
-        option3_button = QPushButton('Option 3', self)
+        option3_button = QPushButton('Fichier ABS', self)
         option3_button.setStyleSheet('QPushButton {background-color: #2196F3; color: white; font-size: 18px;}')
         option3_button.clicked.connect(self.option3Clicked)
         layout.addWidget(option3_button)
+
+        layout.addItem(spacer_item)
 
         close_button = QPushButton('Close', self)
         close_button.setStyleSheet('QPushButton {background-color: #555555; color: white; font-size: 16px;}')
